68a8428451b8255ad293844e22ffa3558d9e8143
Jane <994824764@qq.com>
2023-06-15 11:32:02 +0800

    GameControl使蛇移动

--- code/snake/src/modules/GameControl.ts
+++ code/snake/src/modules/GameControl.ts
@@ -9,6 +9,8 @@ class GameControl{
     scorePanel: ScorePanel;
     // 存储蛇的移动方向（按键方向）
     direction: string = '';
+    // 记录游戏是否结束
+    isLive = true;
 
 
     constructor(){
@@ -24,6 +26,8 @@ class GameControl{
         // 绑定键盘按键按下的事件
         // document.addEventListener('keydown',this.keydownHandler) // 这里的this是document，需要是gamecontrol对象       
         document.addEventListener('keydown',this.keydownHandler.bind(this))
+        // 调用run方法，使蛇移动
+        this.run();
     }
 
     // 键盘按下的响应函数
@@ -33,6 +37,53 @@ class GameControl{
         this.direction = event.key;
     }    
 
+    // 控制蛇移动的方法
+    run(){
+         /*
+        *   根据方向（this.direction）来使蛇的位置改变
+        *       向上 top 减少
+        *       向下 top 增加
+        *       向左  left 减少
+        *       向右  left 增加
+        * */
+        // 获取蛇坐标
+        let X = this.snake.X;
+        let Y = this.snake.Y;
+
+         // 根据按键方向来修改X值和Y值
+         switch (this.direction) {
+            case "ArrowUp":
+            case "Up":
+                // 向上移动 top 减少
+                Y -= 10;
+                break;
+            case "ArrowDown":
+            case "Down":
+                // 向下移动 top 增加
+                Y += 10;
+                break;
+            case "ArrowLeft":
+            case "Left":
+                // 向左移动 left 减少
+                X -= 10;
+                break;
+            case "ArrowRight":
+            case "Right":
+                // 向右移动 left 增加
+                X += 10;
+                break;
+        }
+
+        //修改蛇的X和Y值
+        this.snake.X = X;
+        this.snake.Y = Y;
+
+        
+        this.isLive && setTimeout(this.run.bind(this),300 - (this.scorePanel.level - 1)*30);
+
+
+    }
 
 }
+
 export default GameControl;
